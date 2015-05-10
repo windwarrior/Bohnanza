343edaa0309a5f083c6a6722bf1ad52f65a4ea2b
JElsinga <jochem.elsinga@gmail.com>
2015-05-10 20:44:30 +0200

    New PileTest and CardTest with implementation of respective classes.

--- tests/nl/utwente/bpsd/JunitTestSuite.java
+++ tests/nl/utwente/bpsd/JunitTestSuite.java
@@ -3,8 +3,10 @@ package nl.utwente.bpsd;
 import nl.utwente.bpsd.impl.command.DefaultBuyFieldCommandTest;
 import nl.utwente.bpsd.impl.command.DefaultDrawHandCommandTest;
 import nl.utwente.bpsd.impl.command.DefaultDrawTradeCommandTest;
+import nl.utwente.bpsd.model.CardTest;
 import nl.utwente.bpsd.model.CardTypeTest;
 import nl.utwente.bpsd.model.PlayerTest;
+import nl.utwente.bpsd.model.pile.PileTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -13,6 +15,8 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        PileTest.class,
+        CardTest.class,
         CardTypeTest.class,
         PlayerTest.class,
         DefaultBuyFieldCommandTest.class,
